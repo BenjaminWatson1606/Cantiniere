1e817ac51c56557da0131cc45fcc00c65fc84e23
WATSON Benjamin <benjamin.watson1606@gmail.com>
2024-02-19 16:41:02 +0100

    Project Start

---
+++ Cantiniere/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Cantiniere',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
