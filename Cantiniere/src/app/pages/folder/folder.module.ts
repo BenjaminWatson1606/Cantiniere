76cd65f17d640e50f43e4cd4a1185d21e9c2bc5e
Florian Lernould <florianlernould7@gmail.com>
2024-03-09 15:51:51 +0100

    Add cart pop up to main page module

--- Cantiniere/src/app/pages/folder/folder.module.ts
+++ Cantiniere/src/app/pages/folder/folder.module.ts
@@ -8,9 +8,10 @@ import { FolderPage } from './folder.page';
 import { LoginComponent } from '../../components/auth/login/login.component';
 import { RegisterComponent } from '../../components/auth/register/register.component'; 
 import { PopUpOrderComponent } from 'src/app/components/pop-up-order/pop-up-order.component';
+import { PopUpCartComponent } from 'src/app/components/pop-up-cart/pop-up-cart.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
-  declarations: [FolderPage, LoginComponent, RegisterComponent, PopUpOrderComponent], 
+  declarations: [FolderPage, LoginComponent, RegisterComponent, PopUpOrderComponent, PopUpCartComponent], 
 })
 export class FolderPageModule {}
