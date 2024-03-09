396075ca7b8977db51ef453056860afb251aeb5d
Florian Lernould <florianlernould7@gmail.com>
2024-03-09 15:50:14 +0100

    Add order recap admin page

---
+++ Cantiniere/src/app/pages/admin-order-recap/admin-recap.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-recap',
+  templateUrl: './admin-recap.page.html',
+  styleUrls: ['./admin-recap.page.scss'],
+})
+export class AdminRecapPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  //TODO FAIRE LA FONCTION SI EMPORTE OU NON
+  //TODO FAIRE LA FONCTION ANNULER UNE COMMANDE-->
+
+
+
+
+}
