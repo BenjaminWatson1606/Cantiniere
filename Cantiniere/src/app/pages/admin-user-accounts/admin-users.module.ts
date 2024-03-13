499357e13abfc7e6b74d72fe465ab74914872554
Florian Lernould <florianlernould7@gmail.com>
2024-03-14 00:50:13 +0100

    Update asmin users page
    Get all users from the database using the usersService
    Apply search bar filter to modify the template
    
    (TODO) Show user detail by clicking on the details button

--- Cantiniere/src/app/pages/admin-user-accounts/admin-users.module.ts
+++ Cantiniere/src/app/pages/admin-user-accounts/admin-users.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { AdminUsersPageRoutingModule } from './admin-users-routing.module';
-
 import { AdminUsersPage } from './admin-users.page';
 import { AdminUsersDetailsComponent } from 'src/app/components/admin-users-details/admin-users-details.component';
 
@@ -14,7 +12,7 @@ import { AdminUsersDetailsComponent } from 'src/app/components/admin-users-detai
     CommonModule,
     FormsModule,
     IonicModule,
-    AdminUsersPageRoutingModule
+    AdminUsersPageRoutingModule,
   ],
   declarations: [AdminUsersPage, AdminUsersDetailsComponent]
 })
--- Cantiniere/src/app/pages/admin-user-accounts/admin-users.page.ts
+++ Cantiniere/src/app/pages/admin-user-accounts/admin-users.page.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { AdminUsersDetailsComponent } from 'src/app/components/admin-users-details/admin-users-details.component';
 
+import { User } from 'src/app/interfaces/user';
+import { UsersService } from 'src/app/services/users/users.service';
+
 @Component({
   selector: 'app-admin-users',
   templateUrl: './admin-users.page.html',
@@ -9,9 +12,21 @@ import { AdminUsersDetailsComponent } from 'src/app/components/admin-users-detai
 })
 export class AdminUsersPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  users: User[] = [];
+  searchResult: User[] = [];
+  searchTerm!: string;
+
+  constructor(
+    private modalController: ModalController,
+    private usersService: UsersService,
+  ) { }
 
   ngOnInit() {
+    //Get users from database
+    this.usersService.getAllUsers()?.subscribe(
+      res => this.users = res,
+      error => console.error(error)
+    );
   }
 
   // Open the user details pop up
@@ -22,7 +37,18 @@ export class AdminUsersPage implements OnInit {
     return await modal.present();
   }
 
+  /**
+   * Get the user list to display on template
+   * @returns Returns an array of users
+   */
+  getTemplateList(): User[]{
+    return this.searchResult.length > 0 ? this.searchResult : this.users;
+  }
 
-
+  /**
+   * Apply search bar filter and get an array of user as result
+   */
+  applyFilter(){
+    this.searchResult = this.users.filter(u => u.name === this.searchTerm || u.firstname === this.searchTerm);
+  }
 }
-//TODO FAIRE LA FONCTION RECHERCHER UN UTILISATEUR
